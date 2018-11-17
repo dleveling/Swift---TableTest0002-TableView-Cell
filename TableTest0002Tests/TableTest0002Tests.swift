33aa9c5e2860ee03b290f3eeb5feaab42426e85e
Chaiwat Sombatthepsut <devleveling199x@gmail.com>
2018-11-17 14:42:11 +0700

    Initial Commit

---
+++ TableTest0002Tests/TableTest0002Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TableTest0002Tests.swift
+//  TableTest0002Tests
+//
+//  Created by mercury on 17/11/2561 BE.
+//  Copyright © 2561 mercury. All rights reserved.
+//
+
+import XCTest
+@testable import TableTest0002
+
+class TableTest0002Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
